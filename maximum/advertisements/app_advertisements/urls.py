fb655e19e3d1fa851f4a6ad99683615194c655b4
Mellisa2636 <142491255+Mellisa2636@users.noreply.github.com>
2023-09-30 10:03:49 +0300

    основа проекта

--- maximum/advertisements/app_advertisements/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index, top_sellers
-
-urlpatterns = [
-    path('', index, name='main_page'),
-    path('top_sellers/', top_sellers, name='top_sellers')
-]
